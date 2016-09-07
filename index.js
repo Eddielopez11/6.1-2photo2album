cf814969b90b6c2259a34e4d7ebfad55c1ead0ce
Eduardo Lopez <eddielopez2101@gmail.com>
2016-09-07 13:38:32 -0500

    initial refactor, renaming files and deleting routes

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
-import MyAlbums from "./lib/MyAlbums"
-import Albums from "./lib/Albums"
-import Album from "./lib/Album"
+import Main from "./lib/Main.js"
 
 render((
-  <Router history={ hashHistory }>
-    <Route path="/" component={ MyAlbums }/>
-    <Route path="/albums" component={ Albums }>
-      <Route path="/album/:imgOne" component={ Album }/>
-    </Route>
-  </Router>
+  <Main name="PhotoAlbums"/>
 ), document.getElementById('app'))
